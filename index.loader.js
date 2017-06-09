f7fcec0ca48bcd1ae8a081291e8dbc479a5aaa9f
Hong <plantvsbird@gmail.com>
2017-06-09 14:35:01 +0800

    fix extra semi colon

--- index.loader.js
+++ index.loader.js
@@ -3,7 +3,7 @@ module.exports.pitch = function(request) {
 	this.cacheable();
 	var items = JSON.parse(this.query.substr(1));
 	items = items.map(function(item) {
-		return "require(" + JSON.stringify("-!" + item + "!" + request) + ");";
+		return "require(" + JSON.stringify("-!" + item + "!" + request) + ")";
 	});
 	return items.join("\n") + "\nmodule.exports = " + "[" + items.join(",") + "]";
 };
